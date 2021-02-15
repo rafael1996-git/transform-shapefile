f734cf79d2b594bba4332c0b975460391bdb457e
alejodevelo <alejo_65348@outlook.es>
2021-02-15 15:12:55 -0600

    autor ALejandro Sandoval Rodriguez

--- droptabasebged.sh
+++ droptabasebged.sh
@@ -1,3 +1,4 @@
+#Script elimina todas las base de datos que empiezan con bged Autor: Alejandro Sandoval Rodriguez
 #!/bin/bash
 
 psql -U postgres -d postgres -c "copy (SELECT 'drop database IF EXISTS '||pg_database.datname||';' FROM pg_database JOIN pg_shadow ON pg_database.datdba = pg_shadow.usesysid where datname like 'bged%' order by pg_database.datname) to '/var/lib/pgsql/reestructuracionbged/dropdatabase.sql';"
